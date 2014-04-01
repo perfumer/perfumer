76ea049a83a5a14ae8446ee2b89d6f4699c40b4b
blumfontein <mehmatovec@gmail.com>
2014-04-01 03:46:53 +0000

    Removing templates from exception controller for json requests

--- app/controller/Exception/JsonController.php
+++ app/controller/Exception/JsonController.php
@@ -8,17 +8,21 @@ class JsonController extends BaseController
 {
     public function pageNotFound()
     {
+        $this->error_message = 'Page not found.';
     }
 
     public function isLogged()
     {
+        $this->error_message = 'Access to this page is permitted to logged in users only.';
     }
 
     public function isAdmin()
     {
+        $this->error_message = 'Access to this page is permitted to administrators only.';
     }
 
     public function isGranted()
     {
+        $this->error_message = 'You do not have enough rights to access this page.';
     }
 }
