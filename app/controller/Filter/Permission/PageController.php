3e270a5555a5c5b0983ba507be577f0078bfc742
blumfontein <mehmatovec@gmail.com>
2014-09-08 16:59:11 +0000

    Permission controller divided to 2 separate controllers

---
+++ app/controller/Filter/Permission/PageController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller\Filter\Permission;
+
+use Perfumer\Controller\CoreController;
+
+class PageController extends CoreController
+{
+    public function isLogged()
+    {
+        if (!$this->getUser()->isLogged())
+            $this->getProxy()->forward('exception/page', 'isLogged');
+    }
+
+    public function isAdmin()
+    {
+        $this->isLogged();
+
+        if (!$this->getUser()->isAdmin())
+            $this->getProxy()->forward('exception/page', 'isAdmin');
+    }
+
+    public function isGranted($permissions)
+    {
+        $this->isLogged();
+
+        if (!$this->getUser()->isGranted($permissions))
+            $this->getProxy()->forward('exception/page', 'isGranted');
+    }
+}
