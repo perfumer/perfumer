6d23f3a1c62134378ca629ed99ccf12abeaec178
blumfontein <mehmatovec@gmail.com>
2014-11-14 12:51:01 +0600

    Mark all default tables with leading underscore

--- app/controller/Layout/ApiController.php
+++ app/controller/Layout/ApiController.php
@@ -2,14 +2,14 @@
 
 namespace App\Controller\Layout;
 
-use App\Model\ApiApplicationQuery;
+use App\Model\ApplicationQuery;
 use Perfumer\Controller\JsonController;
 
 class ApiController extends JsonController
 {
     protected $_auth_service_name = 'auth.api';
 
-    protected $api_application;
+    protected $_application;
 
     protected function before()
     {
@@ -20,9 +20,9 @@ class ApiController extends JsonController
         if (!isset($_SERVER[$secret_header]))
             $this->getProxy()->forward('exception/api', 'apiSecretRequired');
 
-        $this->api_application = ApiApplicationQuery::create()->findOneByToken($_SERVER[$secret_header]);
+        $this->_application = ApplicationQuery::create()->findOneByToken($_SERVER[$secret_header]);
 
-        if (!$this->api_application)
+        if (!$this->_application)
             $this->getProxy()->forward('exception/api', 'apiSecretInvalid');
     }
 }
